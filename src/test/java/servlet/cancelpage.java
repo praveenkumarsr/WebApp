55573e8140be7e04f68e41a11b23a61ee1f02632
praveenkumarsr <praveenkumarsr@users.noreply.github.com>
2020-04-11 11:28:22 -0400

    updated the ip address to aws-qa

--- src/test/java/servlet/cancelpage.java
+++ src/test/java/servlet/cancelpage.java
@@ -17,7 +17,7 @@ public class cancelpage extends HttpServlet {
 				try
 				{
 					Class.forName("org.postgresql.Driver");
-					String jdbcUrl="jdbc:postgresql://3.18.107.85:5432/postgres";
+					String jdbcUrl="jdbc:postgresql://18.219.86.223:5432/postgres";
 					String username="postgres";
 					String password="";
 					
